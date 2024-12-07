b2bdc94bd6f96ecc7294f3a8c556e4bf31a9d24d
russell6ix <russellnsiah27@gmail.com>
2024-12-07 13:06:22 +0000

    Add files via upload

---
+++ portfolio/index.js
@@ -0,0 +1,39 @@
+
+// Setup and start animation! 
+  let typed = new Typed('#typed', {
+    strings: ['Frontend developer', 'Backend Developer','Web Designer'],
+    typeSpeed: 50,
+    loop: true,
+  });
+
+const count = document.querySelector(".countings")
+  // Facts counter
+  $('.countings').counterUp({
+    delay: 5,
+    time: 2000
+});
+
+// $(document).ready(function () {
+//     $('.countings').counterUp({
+//         delay: 5,
+//         time: 2000
+//     });
+// });
+
+// go to top 
+const togtop = () =>
+    document.querySelector(".scroll-up").style.
+    display = window.scrollY >= 800 ? "flex" :
+    "none" ;
+
+    window.addEventListener("scroll", togtop);
+    window.addEventListener("resize", togtop);
+
+    // aos
+// AOS.init();
+
+AOS.init({
+    once: true,
+    // offset: 200,
+    // duration: 600,
+});
