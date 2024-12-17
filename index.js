b90d48e91cefe2f055e2e412e827fe982e044029
russell6ix <russellnsiah27@gmail.com>
2024-12-17 14:53:30 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -37,3 +37,11 @@ AOS.init({
     // offset: 200,
     // duration: 600,
 });
+
+// Initialize all popovers
+document.addEventListener('DOMContentLoaded', function () {
+    const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    popoverTriggerList.forEach(function (popoverTriggerEl) {
+        new bootstrap.Popover(popoverTriggerEl);
+    });
+});
